b22f5b86be8f945a8a208e8bda1d5d80ecd5f4fc
Yopi Angi <yopiangi@gmail.com>
2023-02-25 22:48:22 +0700

    bump up version number of the following modules web_view_google_map, web_view_google_map_drawing, web_widget_google_map

--- web_widget_google_map/__manifest__.py
+++ web_widget_google_map/__manifest__.py
@@ -13,7 +13,7 @@
     'website': 'https://github.com/mithnusa',
     'support': 'yopiangi@gmail.com',
     'category': 'Extra Tools',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.1.0',
     'depends': ['base_google_map'],
     'assets': {
         'web.assets_backend': [
