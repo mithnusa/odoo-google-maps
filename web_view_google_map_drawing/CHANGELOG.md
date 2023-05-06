eb8c257355578de3d2da3213ab08be15f9839102
Yopi Angi <yopiangi@gmail.com>
2023-05-06 15:35:53 +0700

    [IMP] Improved widget google_map_drawing

--- web_view_google_map_drawing/CHANGELOG.md
+++ web_view_google_map_drawing/CHANGELOG.md
@@ -1,5 +1,12 @@
 # Change Log
+[16.0.1.3.1] -- 06/05/2023
+### Added
+- Added indicator when a shape is modified or new shape is drawn
 
+### Changed
+### Fixed
+
+---
 ## [16.0.1.2.1] -- 16/04/2023
 ### Added
 - Added Polygon paths measurement information
@@ -9,8 +16,7 @@
 
 ### Fixed
 
-
-
+---
 ## [16.0.1.1.1] -- 12/04/2023
 ### Added
 
--- web_view_google_map_drawing/static/src/widget/google_map_drawing/google_map_drawing.js
+++ web_view_google_map_drawing/static/src/widget/google_map_drawing/google_map_drawing.js
@@ -3,6 +3,7 @@
 import { registry } from '@web/core/registry';
 import { _lt } from '@web/core/l10n/translation';
 import { onWillUpdateProps } from '@odoo/owl';
+import { useDebounced } from '@web/core/utils/timing';
 import { standardFieldProps } from '@web/views/fields/standard_field_props';
 import { renderToString } from '@web/core/utils/render';
 
@@ -15,6 +16,22 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
         this.displayColor = '#006ee5';
         this.customControl = null;
         this.selectedShape = null;
+        this.isEditing = false;
+
+        this._handleDrawPolygonAddListenerDebounce = useDebounced(
+            this._handlePolygonBoundsChanged,
+            500
+        );
+
+        this._handleDrawCircleAddListenerDebounce = useDebounced(
+            this._handleCircleBoundsChanged,
+            500
+        );
+
+        this._handleDrawRectangleAddListenerDebounce = useDebounced(
+            this._handleRectangleBoundsChanged,
+            500
+        );
 
         onWillUpdateProps((_nextProps) => {
             // on page changed, disable the shape already drawn before
@@ -27,6 +44,9 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
                     }
                 }
             }
+            // reset the edit mode
+            this.isEditing = false;
+            this.toggleSaveButtonAnimation();
         });
     }
 
@@ -178,7 +198,32 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
         polygon.setOptions(options);
         this._handleDrawPolygonPoints(shape.lines);
         this._handleCenterMap(polygon.getPath());
+        this._handleDrawPolygonAddListener(polygon);
         return polygon;
+    }
+
+    _handleDrawPolygonAddListener(polygon) {
+        google.maps.event.addListener(
+            polygon.getPath(),
+            'insert_at',
+            this._handleDrawPolygonAddListenerDebounce.bind(this)
+        );
+        // event `dragend` is also fired at event `set_at`
+        google.maps.event.addListener(
+            polygon.getPath(),
+            'set_at',
+            this._handleDrawPolygonAddListenerDebounce.bind(this)
+        );
+        google.maps.event.addListener(
+            polygon.getPath(),
+            'remove_at',
+            this._handleDrawPolygonAddListenerDebounce.bind(this)
+        );
+    }
+
+    _handlePolygonBoundsChanged() {
+        this.isEditing = true;
+        this.toggleSaveButtonAnimation();
     }
 
     _handleDrawRectangle(options) {
@@ -194,7 +239,20 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
         });
         rectangle.setOptions(options);
         this._handleCenterMap(false, rectangle.getBounds());
+        this._handleDrawRectangleAddListener(rectangle);
         return rectangle;
+    }
+
+    _handleDrawRectangleAddListener(rectangle) {
+        rectangle.addListener(
+            'bounds_changed',
+            this._handleDrawRectangleAddListenerDebounce.bind(this)
+        );
+    }
+
+    _handleRectangleBoundsChanged() {
+        this.isEditing = true;
+        this.toggleSaveButtonAnimation();
     }
 
     _handleDrawCircle(options) {
@@ -210,7 +268,24 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
         });
         circle.setOptions(options);
         this._handleCenterMap(false, circle.getBounds());
+        this._handleDrawCircleAddListener(circle);
         return circle;
+    }
+
+    _handleDrawCircleAddListener(circle) {
+        circle.addListener(
+            'radius_changed',
+            this._handleDrawCircleAddListenerDebounce.bind(this)
+        );
+        circle.addListener(
+            'center_changed',
+            this._handleDrawCircleAddListenerDebounce.bind(this)
+        );
+    }
+
+    _handleCircleBoundsChanged() {
+        this.isEditing = true;
+        this.toggleSaveButtonAnimation();
     }
 
     _getGeneralOptions() {
@@ -239,6 +314,28 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
             fillOpacity: 0.45,
             editable: true,
         };
+    }
+
+    toggleSaveButtonAnimation() {
+        this.googleMap.controls[google.maps.ControlPosition.BOTTOM_CENTER].forEach(
+            (element) => {
+                if (element.id === 'custom-control-drawing-buttons') {
+                    let button = element.querySelector('#save');
+                    let floppyIcon = element.querySelector('.fa-floppy-o');
+                    if (button && floppyIcon) {
+                        if (this.isEditing) {
+                            button.classList.add('btn-success');
+                            button.classList.remove('btn-secondary');
+                            floppyIcon.classList.add('animate');
+                        } else {
+                            floppyIcon.classList.remove('btn-success');
+                            button.classList.add('btn-secondary');
+                            floppyIcon.classList.remove('animate');
+                        }
+                    }
+                }
+            }
+        );
     }
 
     _renderMapCustomControl() {
@@ -320,6 +417,8 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
                 type: 'info',
             });
             this.selectedShape = null;
+            this.isEditing = false;
+            this.toggleSaveButtonAnimation();
         }
     }
 
@@ -520,7 +619,8 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
             );
             return;
         }
-
+        this.isEditing = true;
+        this.toggleSaveButtonAnimation();
         shape.type = event.type;
         this.handleSetSelectedShape(shape);
 
@@ -534,7 +634,7 @@ export class GoogleMapDrawing extends GoogleMapDrawingRenderer {
     handleSetSelectedShape(shape) {
         this.selectedShape = shape;
         const options = this._getSelectedOptions();
-        if (['circle', 'rectangle'].indexOf(this.selectedShape.type) >= 0) {
+        if (['circle', 'rectangle', 'polygon'].indexOf(this.selectedShape.type) >= 0) {
             options.draggable = true;
         }
         this.selectedShape.setOptions(options);
