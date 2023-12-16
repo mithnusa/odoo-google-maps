fb42e93f3627c295c0c6f732d8049effb3fd5c7a
Yopi Angi <yopiangi@gmail.com>
2023-12-16 15:40:38 +0800

    web_view_google_map: bug fixes and improvement (for more detail see CHANGELOG.md)

--- web_view_google_map/static/src/views/google_map_form/google_map_form_arch_parser.js
+++ web_view_google_map/static/src/views/google_map_form/google_map_form_arch_parser.js
@@ -1,7 +1,6 @@
 /** @odoo-module **/
 
 import { FormArchParser } from '@web/views/form/form_arch_parser';
-import { archParseBoolean } from '@web/views/utils';
 
 export class GoogleMapFormArchParser extends FormArchParser {
     parse(arch, models, modelName) {
@@ -11,20 +10,3 @@ export class GoogleMapFormArchParser extends FormArchParser {
         return res;
     }
 }
-
-FormArchParser.prototype.originalParse = FormArchParser.prototype.parse;
-
-Object.assign(FormArchParser.prototype, {
-    parse(arch, models, modelName) {
-        const res = FormArchParser.prototype.originalParse.call(this, arch, models, modelName);
-        const activeActions = res.activeActions;
-        activeActions.editGeolocation = archParseBoolean(
-            res.xmlDoc.getAttribute('edit_lat_lng'),
-            false
-        );
-        if (activeActions.editGeolocation) {
-            res.googleMapFormViewRef = res.xmlDoc.getAttribute('google_map_form_view_ref');
-        }
-        return res;
-    },
-});
