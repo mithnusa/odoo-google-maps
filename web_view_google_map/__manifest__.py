0c85af89e169c9c5c24cdcd028778b6f6c39f6f2
Yopi Angi <yopiangi@gmail.com>
2024-06-05 10:14:47 +0700

    [IMP] web_view_google_map: see CHANGELOG.md (#63)

--- web_view_google_map/__manifest__.py
+++ web_view_google_map/__manifest__.py
@@ -14,7 +14,7 @@
     'website': 'https://github.com/mithnusa',
     'support': 'yopiangi@gmail.com',
     'category': 'Extra Tools',
-    'version': '17.0.1.0.1',
+    'version': '17.0.1.0.2',
     'depends': ['base_google_map'],
     'data': ['data/gmap_libraries.xml'],
     'assets': {
