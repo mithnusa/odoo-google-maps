d637608e927406303550160a7db5e671d00a4fd0
Yopi Angi <yopiangi@gmail.com>
2024-06-16 00:11:22 +0700

    [IMP] crm_google_map
    - New google_map sub-view `google_map_crm`. This new sub-view will allow us to update the information shown on marker info window and the sidebar.
    - Added an overlay to the marker, which will show a brief information of the marker when hovered.

---
+++ crm_google_map/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change log
+
+## 17.0.1.1.0
+- New google_map sub-view `google_map_crm`. This new sub-view will allow us to update the information shown on marker info window and the sidebar.
+- Added an overlay to the marker, which will show a brief information of the marker when hovered.
--- crm_google_map/__manifest__.py
+++ crm_google_map/__manifest__.py
@@ -13,12 +13,17 @@
     'website': 'https://github.com/mithnusa',
     'support': 'yopiangi@gmail.com',
     'category': 'Sales/CRM',
-    'version': '17.0.1.0.0',
+    'version': '17.0.1.1.0',
     'depends': [
         'crm',
         'web_view_google_map',
     ],
     'data': ['views/crm_lead.xml'],
+    'assets': {
+        'web.assets_backend': [
+            'crm_google_map/static/src/views/**/*',
+        ]
+    },
     'demo': [],
     'installable': True,
     'application': False,
