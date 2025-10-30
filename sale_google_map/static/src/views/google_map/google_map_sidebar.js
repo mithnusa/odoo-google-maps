0e5a21d66044f32319d6ab681027f6d674011d59
Yopi Angi <yopiangi@gmail.com>
2025-10-30 10:33:15 +0700

    [19.0] Migration: web_view_google_map, sale_google_map: improvement

--- sale_google_map/static/src/views/google_map/google_map_sidebar.js
+++ sale_google_map/static/src/views/google_map/google_map_sidebar.js
@@ -3,15 +3,16 @@ import { useService } from '@web/core/utils/hooks';
 import { GoogleMapSidebar } from '@web_view_google_map/views/google_map/google_map_sidebar';
 
 export class GoogleMapSidebarSaleOrder extends GoogleMapSidebar {
+    static template = 'sale_google_map.GoogleMapSidebar';
     static groupItemTemplate = 'sale_google_map.GroupItem';
 
     setup() {
         super.setup();
         this.uiService = useService('ui');
-        onMounted(this._loadGroupRecord);
+        onMounted(this.loadGroupRecord);
     }
 
-    async _loadGroupRecord() {
+    async loadGroupRecord() {
         const datas = this.props.getGroupsOrRecords();
         if (this.props.isGrouped && datas.length > 0) {
             const groupPromises = datas.map(async ({ group }) => {
