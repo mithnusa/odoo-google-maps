45d08eb163c9713f42c4ced474b1a4b42fe683e1
Yopi Angi <yopiangi@gmail.com>
2025-11-05 12:19:12 +0700

    [19.0] contacts_google_map improvement (#73)
    * sale_google_map: small UI improvement for marker info window
    
    * web_view_google_map_drawing: small UI improvement for feature highlight information
    
    * [IMP] web_view_google_map: removed unused method; small UI improvement
    
    * [IMP] contacts_google_map: see CHANGELOG.md

--- contacts_google_map/__manifest__.py
+++ contacts_google_map/__manifest__.py
@@ -13,7 +13,7 @@
     'website': 'https://github.com/mithnusa',
     'support': 'yopiangi@gmail.com',
     'category': 'Sales/CRM',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'depends': [
         'base_geolocalize',
         'contacts',
