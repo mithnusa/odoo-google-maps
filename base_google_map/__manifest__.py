f721f714930c5dbd310203dff9edf7950c832695
Yopi Angi <yopiangi@gmail.com>
2026-02-01 19:07:18 +0800

    [IMP] base_google_map: Enable scale control on Google Map (#96)

--- base_google_map/__manifest__.py
+++ base_google_map/__manifest__.py
@@ -13,7 +13,7 @@
     'website': 'https://github.com/mithnusa',
     'support': 'yopiangi@gmail.com',
     'category': 'Extra Tools',
-    'version': '1.0.4',
+    'version': '1.0.5',
     'depends': ['web', 'base_geolocalize'],
     'data': [
         'data/google_map.xml',
