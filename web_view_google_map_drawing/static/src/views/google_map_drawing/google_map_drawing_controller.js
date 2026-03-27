268af228e59acb9e59631c166bd97668aa804079
Yopi Angi <yopiangi@gmail.com>
2026-03-27 15:21:03 +0700

    [IMP] web_view_google_map_drawing: Remove showNearbyRecords prop from drawing mode
    - Override `rendererProps` in `GoogleMapDrawingController` to strip
      `showNearbyRecords`, which is not applicable in drawing mode
    - Exclude `showNearbyRecords` from `GoogleMapsDrawingSidebar.props`
      by deriving from `GoogleMapSidebar.props` and deleting the key

--- web_view_google_map_drawing/static/src/views/google_map_drawing/google_map_drawing_controller.js
+++ web_view_google_map_drawing/static/src/views/google_map_drawing/google_map_drawing_controller.js
@@ -1,4 +1,3 @@
-import { _t } from '@web/core/l10n/translation';
 import { GoogleMapController } from '@web_view_google_map/views/google_map/google_map_controller';
 
 
@@ -13,5 +12,14 @@ export class GoogleMapDrawingController extends GoogleMapController {
         return Object.assign({}, viewConfig, {
             geoJsonField: this.archInfo.geoJsonField,
         });
+    }
+
+    get rendererProps() {
+        const rendererProps = super.rendererProps;
+        // remove unnecessary properties
+        // following properties are not used in drawing mode
+        delete rendererProps.showNearbyRecords;
+
+        return rendererProps;
     }
 }
--- web_view_google_map_drawing/static/src/views/google_map_drawing/google_map_drawing_sidebar.js
+++ web_view_google_map_drawing/static/src/views/google_map_drawing/google_map_drawing_sidebar.js
@@ -1,7 +1,11 @@
-import { _t } from '@web/core/l10n/translation';
 import { GoogleMapSidebar } from '@web_view_google_map/views/google_map/google_map_sidebar';
+
+const googleMapSidebarProps = { ...GoogleMapSidebar.props };
+// remove unnecessary properties
+delete googleMapSidebarProps.showNearbyRecords;
 
 export class GoogleMapsDrawingSidebar extends GoogleMapSidebar {
     static recordItemTemplate = 'web_view_google_map_drawing.RecordItem';
     static groupItemTemplate = 'web_view_google_map_drawing.GroupItem';
+    static props = { ...googleMapSidebarProps };
 }
